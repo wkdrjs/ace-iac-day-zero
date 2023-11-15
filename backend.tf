7ee8e8e3676a7bd613d8af61e97305475849aeb6
wkdrjs <96409282+wkdrjs@users.noreply.github.com>
2023-11-15 10:59:16 +0900

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "aviatrix-iac_zhangjian"
+    organization = "aviatrix-iac_zhangjian"
     workspaces {
       name = "ace-iac-day-zero"
     }
