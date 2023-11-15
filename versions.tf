4fa8259ee7c48ec99a746c43ea0f5e133d2edcc5
wkdrjs <96409282+wkdrjs@users.noreply.github.com>
2023-11-15 18:01:46 +0900

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "3.1.3"
+      version = "~> 2.24.0"
     }
     aws = {
       source  = "hashicorp/aws"
